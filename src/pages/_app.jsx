69806eebe7b81301aea7dcad4efe42929ecf540f
Jon Park <jon@jonathanpark.ca>
2023-09-27 22:18:16 -0400

    feat: remove fingerprint

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,7 +3,6 @@ import { useEffect, useRef } from 'react';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
 import PlausibleProvider from 'next-plausible';
-import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react';
 
 import '@/styles/tailwind.css';
 import 'focus-visible';
@@ -32,15 +31,6 @@ export default function App({ Component, pageProps, router }) {
         <Header />
         <main>
           <PlausibleProvider domain="jonathanpark.ca">
-            <FpjsProvider
-              loadOptions={{
-                apiKey: 'Q9Tin0Pcy5Mgsw04VNvQ',
-                scriptUrlPattern: 'https://jonathanpark.ca/39wGgI707VIKmG7U/O7y3kMque0DaVX7s?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>',
-                endpoint: 'https://jonathanpark.ca/39wGgI707VIKmG7U/nyVRNqHm5JGxgESd',
-              }}
-            >
-              <Component previousPathname={previousPathname} {...pageProps} />
-            </FpjsProvider>
           </PlausibleProvider>
         </main>
         <Footer />
